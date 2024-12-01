269bb38a81c2c3215d2b07364b1c8099fe424cb0
Soukaina Jabri <soukaina.jabri333@gmail.com>
2024-12-01 10:04:06 +0100

    fixing tests after merge

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,14 @@
-import { screen, render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { MemoryRouter } from "react-router-dom"; // To simulate routes in tests
 import App from "./App";
 
-describe("App tests", () => {
-    it("should render the title", () => {
-    render(<App />);
-
-    expect(
-        screen.getByRole("heading", {
-        level: 1,
-    })
-    ).toHaveTextContent("Vite + React");
-    });
+describe("App Component", () => {
+  it("should render the Login component for the root path", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}> {/* Simulate the "/" route */}
+        <App />
+      </MemoryRouter>
+    );
+  });
 });
